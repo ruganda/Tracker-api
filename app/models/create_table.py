0ddbff8484aff60428c33e237d163887c27cdf32
ruganda <mubaruganda@gmail.com>
2018-06-07 13:16:50 +0300

    modify the database table

--- app/models/create_table.py
+++ app/models/create_table.py
@@ -1,17 +1,14 @@
 import psycopg2
 
-connection = psycopg2.connect("dbname='tracker_api' user='postgres' host='localhost' password='15december' port ='5432'")
-
+connection = psycopg2.connect(
+       "dbname='tracker_api' user='postgres' host='localhost' password='15december' port ='5432'")
 cursor = connection.cursor()
-
-
 # This is the only place where int vs INTEGER matters—in auto-incrementing columns
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name text, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name text, username text, password text, isAdmin bool)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS requests (id INTEGER PRIMARY KEY, item text, typ text, description text, satatus text)"
+create_table = "CREATE TABLE IF NOT EXISTS requests (id SERIAL PRIMARY KEY, item text, typ text, description text, status text, owner text)"
 cursor.execute(create_table)
 
 connection.commit()
-
 connection.close()
